445cee2df90b224c74503da8d2e746ba4c19ba9c
Sarah Venske Costa <sarahvenske@gmail.com>
2023-01-10 13:48:07 -0300

    forbiddenUpdateDataMiddleware fixed

--- src/middlewares/forbbidenUpdateData.middlewares.ts
+++ src/middlewares/forbbidenUpdateData.middlewares.ts
@@ -4,8 +4,9 @@ import { AppError } from "../errors/AppError";
 const forbbidenUpdateDataMiddleware = async (req: Request, res: Response, next: NextFunction) => {
     
     const userData = req.body
+    const userDataKeys = Object.keys(userData)
 
-    if(Object.keys(userData).includes("id" || "isAdm" || "isActive")){
+    if(userDataKeys.includes("id") || userDataKeys.includes("isAdm") || userDataKeys.includes("isActive")){
         throw new AppError("Unauthorized field update", 401)
     }
 
